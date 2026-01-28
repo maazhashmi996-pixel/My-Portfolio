e8fb2e05df42140b31b53d9aac62347546a77314
Maaz Hashmi <maazhashmi996-pixel>
2026-01-28 19:53:58 +0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/section/hero";
+import Portfolio from "@/components/section/Projects";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Hero />
+
+      <Portfolio />
     </div>
   );
 }
