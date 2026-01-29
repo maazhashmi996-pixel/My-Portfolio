b46c657e562f03c2488e0151689f12ed3a98f470
Maaz Hashmi <maazhashmi996-pixel>
2026-01-29 14:25:51 +0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import ContactForm from "@/components/ContactForm";
-import Hero from "@/components/section/hero";
+import { Hero } from "@/components/section/hero";
 import Portfolio from "@/components/section/Portfolio";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-
+      <Hero />
       <Portfolio />
       <ContactForm />
     </div>
